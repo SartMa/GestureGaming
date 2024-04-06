804981c2b0ce7388f2e328c684fa22048dd60eff
BookwormBoy <35136625+BookwormBoy@users.noreply.github.com>
2024-04-06 22:40:34 +0530

    Update bullet.py
    Added bullet packs that replenish bullet count

--- Game/bullet.py
+++ Game/bullet.py
@@ -1,4 +1,5 @@
 import pygame
+from settings import screen_width
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, pos, lane):
@@ -10,3 +11,15 @@ class Bullet(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.x+=5
+
+class BulletPack(pygame.sprite.Sprite):
+    def __init__(self, lane):
+        super().__init__()
+        self.image = pygame.image.load('./graphics/bullet/tile004.png')
+        # self.image = pygame.transform.scale(self.image, (3*2, 1*2))
+        self.lane=lane
+        self.y_positions={1:320, 2:450, 3: 580}
+        self.rect=self.image.get_rect(topleft= (screen_width, self.y_positions[self.lane]))
+
+    def update(self, shift):
+        self.rect.x-=shift
